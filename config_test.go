d8a14bd3a28175494f361000ca171b83a6e5086c
Cesar Gimenes <crg@crg.eti.br>
2017-01-11 10:03:37 -0200

    draft test

--- config_test.go
+++ config_test.go
@@ -1,1 +1,72 @@
 package goConfig
+
+import (
+	"fmt"
+	"os"
+	"testing"
+)
+
+type testStruct struct {
+	A int    `cfg:"A" cfgDefault:"100"`
+	B string `cfg:"B" cfgDefault:"200"`
+	C string
+	N string `cfg:"-"`
+	p string
+	S testSub `cfg:"S"`
+}
+
+type testSub struct {
+	A int        `cfg:"A" cfgDefault:"300"`
+	B string     `cfg:"C" cfgDefault:"400"`
+	S testSubSub `cfg:"S"`
+}
+type testSubSub struct {
+	A int    `cfg:"A" cfgDefault:"500"`
+	B string `cfg:"S" cfgDefault:"600"`
+}
+
+func TestParse(t *testing.T) {
+
+	os.Setenv("A", "900")
+	os.Setenv("B", "TEST")
+
+	s := &testStruct{A: 1, S: testSub{A: 1, B: "2"}}
+
+	err := Parse(s)
+	if err != nil {
+		t.Fatal(err)
+	}
+
+	fmt.Printf("\n\nTestParseTags: %#v\n\n", s)
+
+	/*
+		os.Setenv("A", "900ERROR")
+
+		goFlags.Reset()
+		err = Parse(s)
+		if err == nil {
+			t.Fatal("Error expected")
+		}
+
+		os.Setenv("A", "")
+
+		goFlags.Reset()
+		err = Parse(s)
+		if err != nil {
+			t.Fatal(err)
+		}
+
+		s1 := "test"
+		goFlags.Reset()
+		err = Parse(s1)
+		if err == nil {
+			t.Fatal("Error expected")
+		}
+
+		goFlags.Reset()
+		err = Parse(&s1)
+		if err == nil {
+			t.Fatal("Error expected")
+		}
+	*/
+}
