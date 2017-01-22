b51105a3d5804dd0628dd81e4275d0b453f6f62c
Cesar Gimenes <crg@crg.eti.br>
2017-01-22 20:17:43 -0200

    add doc.go file

---
+++ goEnv/doc.go
@@ -0,0 +1,7 @@
+//Copyright (c) 2016 Cesar Gimenes - MIT License
+//
+//goEnv uses a struct as input and populates the fields
+//of this struct with parameters environment variables.
+//
+
+package goEnv
